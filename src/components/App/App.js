757423f535af43c45aa54522a8c70ae5ea03a139
thejeqff <desjeff@gmail.com>
2018-05-31 20:08:30 -0700

    Added remaining components in draft form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import {SearchBar} from '../components/SearchBar';
-import {SearchResults} from '../components/SearchResults';
-import {Playlist} from '../components/Playlist';
+import {SearchBar} from '../SearchBar/SeartchBar';
+import {SearchResults} from '../SearchResults/SearchResults';
+import {Playlist} from '../Playlist/Playlist';
 
 class App extends Component {
   render() {
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TrackList} from '../TrackList/TrackList';
+import './SearchList.css';
+
+export class SearchResults extends React.Component {
+  render() {
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList />
+    </div>
+  }
+}
